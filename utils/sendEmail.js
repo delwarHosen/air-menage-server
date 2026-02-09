925e4ef19a767cf86b63bc076d27c15870ec8e5c
delwarHosen <delwar.hosen008@gmail.com>
2026-02-09 15:14:02 +0600

    nodeMailer security  add

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,6 +3,9 @@ import nodemailer from "nodemailer";
 export const sendEmail = async (to, code) => {
     const transporter = nodemailer.createTransport({
         service: "gmail",
+        host:"smtp.gmail.com",
+        port:587,
+        secure:false,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
